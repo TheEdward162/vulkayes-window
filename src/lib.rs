8d76d337ef594e7ee73e30a7fe33a29b9e6ac353
TheEdward162 <theedward162@gmail.com>
2020-04-17 00:10:10 +0200

    refactor, finish xlib/xcb/wayland

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,206 @@
-use std::ffi::c_void;
+//! Provides platform specific glue between windows and Vulkan.
 
-use vulkayes_core::ash::{self, vk};
+use std::ffi::c_void;
+use vulkayes_core::ash;
+use ash::vk;
+
+pub const UNIX_USE_XCB_DEFAULT: bool = cfg!(feature = "unix_use_xcb_default");
 
 #[cfg(feature = "winit")]
 pub mod winit;
 
-#[cfg(target_os = "macos")]
-pub unsafe fn raw_surface(
-	window_handle: cocoa::base::id,
-	view_handle: cocoa::base::id,
+/// ### Safety
+///
+/// * `window_handle` must be a valid NSWindow handle.
+/// * `view_handle` must be a valid NSView handle. 
+///
+/// Note that while this function can be called on platforms different than macos, it will only 
+/// perform all necessary setup on in conditional compilation on macos.
+#[allow(unused_variables)]
+pub unsafe fn raw_surface_cocoa(
+	window_handle: *mut c_void,
+	view_handle: *const c_void,
 	entry: &ash::Entry,
 	instance: &ash::Instance,
 	allocation_callbacks: Option<&vk::AllocationCallbacks>
 ) -> Result<ash::vk::SurfaceKHR, ash::vk::Result> {
-	use cocoa::appkit::{NSView, NSWindow};
+	
+	#[cfg(target_os = "macos")]
+	{
+		use cocoa::appkit::{NSView, NSWindow};
 
-	let view = window_handle.contentView();
+		let window_handle: cocoa::base::id = std::mem::transmute(window_handle);
+		let view = window_handle.contentView();
 
-	let layer = metal::CoreAnimationLayer::new();
-	layer.set_edge_antialiasing_mask(0);
-	layer.set_presents_with_transaction(false);
-	layer.remove_all_animations();
-	layer.set_contents_scale(view.backingScaleFactor());
+		let layer = metal::CoreAnimationLayer::new();
+		layer.set_edge_antialiasing_mask(0);
+		layer.set_presents_with_transaction(false);
+		layer.remove_all_animations();
+		layer.set_contents_scale(view.backingScaleFactor());
 
-	view.setLayer(std::mem::transmute(layer.as_ref()));
-	view.setWantsLayer(cocoa::base::YES);
+		view.setLayer(
+			std::mem::transmute(layer.as_ref())
+		);
+		view.setWantsLayer(cocoa::base::YES);
+	}
 
 	let mut create_info = vk::MacOSSurfaceCreateInfoMVK::builder();
-	create_info.p_view = view_handle as *const c_void;
+	create_info.p_view = view_handle;
 
-	let macos_surface_loader = ash::extensions::mvk::MacOSSurface::new(entry, instance);
-
-	let surface = macos_surface_loader.create_mac_os_surface_mvk(
+	vulkayes_core::log::info!("Creating macOS surface");
+	let loader = ash::extensions::mvk::MacOSSurface::new(entry, instance);
+	let surface = loader.create_mac_os_surface_mvk(
 		&create_info, allocation_callbacks
 	)?;
 
 	Ok(surface)
 }
-#[cfg(target_os = "macos")]
-pub fn required_surface_extensions() -> impl AsRef<[&'static str]> {
+
+pub fn required_surface_extensions_cocoa() -> [&'static str; 2] {
 	[
 		ash::extensions::khr::Surface::name().to_str().unwrap(),
 		ash::extensions::mvk::MacOSSurface::name().to_str().unwrap()
 	]
 }
 
-#[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-pub fn raw_surface(
-	instance: Vrc<Instance>,
-	window: &Window,
-	host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
-) -> Result<Surface, SurfaceError> {
-	unimplemented!() // TODO: Implement and test on platform
-	// use winit::platform::unix::WindowExtUnix;
 
-	// let x11_display = window.get_xlib_display().unwrap();
-	// let x11_window = window.get_xlib_window().unwrap();
+/// ### Safety
+///
+/// * `x11_window` must be a valid X11 Window handle.
+/// * `x11_display` must be a valid X11 Display handle. 
+pub unsafe fn raw_surface_xlib(
+	x11_window: vk::Window,
+	x11_display: *mut vk::Display,
+	entry: &ash::Entry,
+	instance: &ash::Instance,
+	allocation_callbacks: Option<&vk::AllocationCallbacks>
+) -> Result<ash::vk::SurfaceKHR, ash::vk::Result> {
+	let create_info = vk::XlibSurfaceCreateInfoKHR::builder()
+		.window(x11_window)
+		.dpy(x11_display)
+	;
 
-	// let x11_create_info = vk::XlibSurfaceCreateInfoKHR::builder()
-	// 	.window(x11_window)
-	// 	.dpy(x11_display as *mut vk::Display)
-	// 	.build();
+	vulkayes_core::log::info!("Creating Xlib surface");
+	let loader = ash::extensions::khr::XlibSurface::new(
+		entry,
+		instance
+	);
+	let surface = loader.create_xlib_surface(
+		&create_info,
+		allocation_callbacks
+	)?;
 
-	// let xlib_surface_loader =
-	// 	ash::extensions::khr::XlibSurface::new(instance.entry().deref(), instance.deref().deref());
-
-	// let allocation_callbacks: Option<vk::AllocationCallbacks> = host_memory_allocator.into();
-
-	// let surface =
-	// 	xlib_surface_loader.create_xlib_surface(&x11_create_info, allocation_callbacks.as_ref())?;
-
-	// unsafe {
-	// 	Ok(vulkayes_core::surface::Surface::new(instance, surface, allocation_callbacks))
-	// }
+	Ok(surface)
 }
-#[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-pub fn required_surface_extensions() -> impl AsRef<[&'static str]> {
+pub fn required_surface_extensions_xlib() -> [&'static str; 2] {
 	[
 		ash::extensions::khr::Surface::name().to_str().unwrap(),
-		ash::extensions::mvk::XlibSurface::name().to_str().unwrap()
+		ash::extensions::khr::XlibSurface::name().to_str().unwrap()
+	]
+}
+
+/// ### Safety
+///
+/// * `xcb_connection` must be a valid Xcb connection.
+/// * `xcb_window` must be a valid Xcb window.
+pub unsafe fn raw_surface_xcb(
+	xcb_connection: *mut vk::xcb_connection_t,
+	xcb_window: vk::xcb_window_t,
+	entry: &ash::Entry,
+	instance: &ash::Instance,
+	allocation_callbacks: Option<&vk::AllocationCallbacks>
+) -> Result<ash::vk::SurfaceKHR, ash::vk::Result> {
+	let create_info = vk::XcbSurfaceCreateInfoKHR::builder()
+		.connection(xcb_connection)
+		.window(xcb_window)
+	;
+
+	vulkayes_core::log::info!("Creating Xcb surface");
+	let loader = ash::extensions::khr::XcbSurface::new(
+		entry,
+		instance
+	);
+	let surface = loader.create_xcb_surface(
+		&create_info,
+		allocation_callbacks
+	)?;
+
+	Ok(surface)
+}
+pub fn required_surface_extensions_xcb() -> [&'static str; 2] {
+	[
+		ash::extensions::khr::Surface::name().to_str().unwrap(),
+		ash::extensions::khr::XcbSurface::name().to_str().unwrap()
+	]
+}
+
+/// ### Safety
+///
+/// * `display` must be a valid Wayland display handle.
+/// * `surface` must be a valid Wayland surface handle.
+pub unsafe fn raw_surface_wayland(
+	display: *mut vk::wl_display,
+	surface: *mut vk::wl_surface,
+	entry: &ash::Entry,
+	instance: &ash::Instance,
+	allocation_callbacks: Option<&vk::AllocationCallbacks>
+) -> Result<ash::vk::SurfaceKHR, ash::vk::Result> {
+	let create_info = vk::WaylandSurfaceCreateInfoKHR::builder()
+		.display(display)
+		.surface(surface)
+	;
+
+	vulkayes_core::log::info!("Creating Wayland surface");
+	let loader = ash::extensions::khr::WaylandSurface::new(
+		entry,
+		instance
+	);
+	let surface = loader.create_wayland_surface(
+		&create_info,
+		allocation_callbacks
+	)?;
+
+	Ok(surface)
+}
+pub fn required_surface_extensions_wayland() -> [&'static str; 2] {
+	[
+		ash::extensions::khr::Surface::name().to_str().unwrap(),
+		ash::extensions::khr::WaylandSurface::name().to_str().unwrap()
+	]
+}
+
+/// ### Safety
+///
+/// `instance` must be a valid Win32 instance.
+/// `window_handle` must be a valid Win32 window handle.
+pub unsafe fn raw_surface_win32(
+	window_instance: vk::HINSTANCE,
+	window_handle: vk::HWND,
+	entry: &ash::Entry,
+	instance: &ash::Instance,
+	allocation_callbacks: Option<&vk::AllocationCallbacks>
+) -> Result<ash::vk::SurfaceKHR, ash::vk::Result> {
+	let create_info = vk::Win32SurfaceCreateInfoKHR::builder()
+		.hinstance(window_instance)
+		.hwnd(window_handle)
+	;
+
+	vulkayes_core::log::info!("Creating Win32 surface");
+	let loader = ash::extensions::khr::Win32Surface::new(
+		entry,
+		instance
+	);
+	let surface = loader.create_win32_surface(
+		&create_info,
+		allocation_callbacks
+	)?;
+
+	Ok(surface)
+}
+
+pub fn required_surface_extensions_win32() -> [&'static str; 2] {
+	[
+		ash::extensions::khr::Surface::name().to_str().unwrap(),
+		ash::extensions::khr::Win32Surface::name().to_str().unwrap()
 	]
 }
--- src/winit.rs
+++ src/winit.rs
@@ -1,111 +1,195 @@
-use std::ops::Deref;
-
-use vulkayes_core::{
-	instance::Instance,
-	surface::{error::SurfaceError, Surface},
-	Vrc
-};
-
 pub use winit;
-use winit::window::Window;
+
+pub use inner::*;
 
 #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-pub fn create_surface(
-	instance: Vrc<Instance>,
-	window: &Window,
-	host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
-) -> Result<Surface, SurfaceError> {
-	unimplemented!() // TODO: Implement and test on platform
-	             // use winit::platform::unix::WindowExtUnix;
+mod inner {
+	use std::ops::Deref;
 
-	// let x11_display = window.get_xlib_display().unwrap();
-	// let x11_window = window.get_xlib_window().unwrap();
+	use winit::window::Window;
 
-	// let x11_create_info = vk::XlibSurfaceCreateInfoKHR::builder()
-	// 	.window(x11_window)
-	// 	.dpy(x11_display as *mut vk::Display)
-	// 	.build();
+	use vulkayes_core::{
+		instance::Instance,
+		surface::{error::SurfaceError, Surface},
+		Vrc
+	};
 
-	// let xlib_surface_loader =
-	// 	ash::extensions::khr::XlibSurface::new(instance.entry().deref(), instance.deref().deref());
+	pub fn create_surface(
+		instance: Vrc<Instance>,
+		window: &Window,
+		host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
+	) -> Result<Surface, SurfaceError> {
+		create_surface_ext(instance, window, host_memory_allocator, crate::UNIX_USE_XCB_DEFAULT)
+	}
+	pub fn required_surface_extensions(window: &Window) -> impl AsRef<[&'static str]> {
+		required_surface_extensions_ext(window, crate::UNIX_USE_XCB_DEFAULT)
+	}
 
-	// let allocation_callbacks: Option<vk::AllocationCallbacks> = host_memory_allocator.into();
+	pub fn create_surface_ext(
+		instance: Vrc<Instance>,
+		window: &Window,
+		host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator,
+		use_xcb: bool
+	) -> Result<Surface, SurfaceError> {
+		use winit::platform::unix::WindowExtUnix;
 
-	// let surface =
-	// 	xlib_surface_loader.create_xlib_surface(&x11_create_info, allocation_callbacks.as_ref())?;
+		if let (Some(wayland_display), Some(wayland_surface)) = (window.wayland_display(), window.wayland_surface()) {
+			let surface = unsafe {
+				crate::raw_surface_wayland(
+					wayland_display,
+					wayland_surface,
+					instance.entry().deref(),
+					instance.deref().deref(),
+					host_memory_allocator.as_ref()
+				)? 
+			};
 
-	// unsafe {
-	// 	Ok(vulkayes_core::surface::Surface::new(instance, surface, allocation_callbacks))
-	// }
+			let vy_surface = unsafe {
+				vulkayes_core::surface::Surface::from_existing(instance, surface, host_memory_allocator)
+			};
+
+			return Ok(vy_surface)
+		}
+
+
+		if let Some(x11_window) = window.xlib_window() {
+			let surface = if use_xcb {
+				 unsafe {
+					crate::raw_surface_xcb(
+						window.xcb_connection().unwrap(),
+						x11_window as _,
+						instance.entry().deref(),
+						instance.deref().deref(),
+						host_memory_allocator.as_ref()
+					)? 
+				}
+			} else {
+				unsafe {
+					crate::raw_surface_xlib(
+						x11_window,
+						window.xlib_display().unwrap() as *mut _,
+						instance.entry().deref(),
+						instance.deref().deref(),
+						host_memory_allocator.as_ref()
+					)? 
+				}
+			};
+
+			let vy_surface = unsafe {
+				vulkayes_core::surface::Surface::from_existing(instance, surface, host_memory_allocator)
+			};
+
+			return Ok(vy_surface)
+		}
+
+		unimplemented!("Only implemented for Wayland, Xlib and Xcb")
+	}
+	pub fn required_surface_extensions_ext(window: &Window, use_xcb: bool) -> impl AsRef<[&'static str]> {
+		use winit::platform::unix::WindowExtUnix;
+		
+		if let (Some(_), Some(_)) = (window.wayland_display(), window.wayland_surface()) {
+			return crate::required_surface_extensions_wayland();
+		}
+
+		if let Some(_) = window.xlib_window() {
+			if use_xcb {
+				return crate::required_surface_extensions_xcb();
+			} else {
+				return crate::required_surface_extensions_xlib();
+			}
+		}
+
+		unimplemented!("Only implemented for Wayland, Xlib and Xcb")
+	}
 }
-#[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-pub fn required_surface_extensions() -> impl AsRef<[&'static str]> {
-	[
-		ash::extensions::khr::Surface::name().to_str().unwrap(),
-		ash::extensions::mvk::XlibSurface::name().to_str().unwrap()
-	]
-}
-
 
 #[cfg(target_os = "macos")]
-pub fn create_surface(
-	instance: Vrc<Instance>,
-	window: &Window,
-	host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
-) -> Result<Surface, SurfaceError> {
-	use winit::platform::macos::WindowExtMacOS;
+mod inner {
+	use std::ops::Deref;
 
-	let surface = unsafe {
-		crate::raw_surface(
-			std::mem::transmute(window.ns_window()),
-			std::mem::transmute(window.ns_view()),
-			instance.entry().deref(),
-			instance.deref().deref(),
-			host_memory_allocator.as_ref()
-		)?
+	use winit::window::Window;
+
+	use vulkayes_core::{
+		instance::Instance,
+		surface::{error::SurfaceError, Surface},
+		Vrc
 	};
 
-	let vy_surface = unsafe {
-		vulkayes_core::surface::Surface::from_existing(
-			instance,
-			surface,
-			host_memory_allocator
+	pub fn create_surface(
+		instance: Vrc<Instance>,
+		window: &Window,
+		host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
+	) -> Result<Surface, SurfaceError> {
+		use winit::platform::macos::WindowExtMacOS;
+
+		let surface = unsafe {
+			crate::raw_surface_cocoa(
+				std::mem::transmute(window.ns_window()),
+				std::mem::transmute(window.ns_view()),
+				instance.entry().deref(),
+				instance.deref().deref(),
+				host_memory_allocator.as_ref()
+			)?
+		};
+
+		let vy_surface = unsafe {
+			vulkayes_core::surface::Surface::from_existing(
+				instance,
+				surface,
+				host_memory_allocator
+			)
+		};
+
+		Ok(
+			vy_surface
 		)
-	};
-
-	Ok(
-		vy_surface
-	)
+	}
+	pub fn required_surface_extensions(window: &Window) -> [&'static str; 2] {
+		crate::required_surface_extensions()
+	}
 }
 
 #[cfg(target_os = "windows")]
-unsafe fn create_surface(
-	instance: Vrc<Instance>,
-	window: &Window,
-	host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
-) -> Result<Surface, SurfaceError> {
-	unimplemented!() // TODO: Implement and test on platform
-	             // use std::ptr;
-	             // use winapi::{shared::windef::HWND, um::libloaderapi::GetModuleHandleW};
-	             // use winit::os::windows::WindowExt;
-	             //
-	             // let hwnd = window.get_hwnd() as HWND;
-	             // let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
-	             // let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
-	             // 	s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
-	             // 	p_next: ptr::null(),
-	             // 	flags: Default::default(),
-	             // 	hinstance,
-	             // 	hwnd: hwnd as *const c_void
-	             // };
-	             // let win32_surface_loader = Win32Surface::new(entry, instance);
-	             // win32_surface_loader.create_win32_surface(&win32_create_info, None)
-}
+mod inner {
+	use winit::window::Window;
 
-#[cfg(all(windows))]
-pub fn required_surface_extensions() -> impl AsRef<[&'static str]> {
-	[
-		ash::extensions::khr::Surface::name().to_str().unwrap(),
-		ash::extensions::mvk::Win32Surface::name().to_str().unwrap()
-	]
+	use vulkayes_core::{
+		instance::Instance,
+		surface::{error::SurfaceError, Surface},
+		Vrc
+	};
+
+	unsafe fn create_surface(
+		instance: Vrc<Instance>,
+		window: &Window,
+		host_memory_allocator: vulkayes_core::memory::host::HostMemoryAllocator
+	) -> Result<Surface, SurfaceError> {
+		use winapi::{shared::windef::HWND, um::libloaderapi::GetModuleHandleW};
+		use winit::os::windows::WindowExt;
+
+		let hinstance = GetModuleHandleW(std::ptr::null()) as *const c_void;
+		let hwnd = window.get_hwnd() as HWND;
+
+		let surface = unsafe {
+			crate::raw_surface_win32(
+				hinstance,
+				hwnd,
+				instance.entry().deref(),
+				instance.deref().deref(),
+				host_memory_allocator.as_ref()
+			)?
+		};
+
+		let vy_surface = unsafe {
+			vulkayes_core::surface::Surface::from_existing(
+				instance,
+				surface,
+				host_memory_allocator
+			)
+		};
+
+		Ok(
+			vy_surface
+		)
+	}
 }
